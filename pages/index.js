aaf4c6793a212f19b40a0c89a356693b7fffdfc8
Leon Heuer <leon.heuer@outlook.de>
2022-09-11 16:33:40 +0200

    I hate CORS

--- pages/index.js
+++ pages/index.js
@@ -2,46 +2,63 @@ import Head from 'next/head'
 import { useState } from 'react';
 
 export default function Home() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState("");
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState("");
 
-  function login(event) {
-    event.preventDefault();
-    
-  }
+	async function login(event) {
+		event.preventDefault();
+		
+		const response = await fetch("http://heuer.ovh:8080/whatsflat/users/try_login", {
+			method: "POST",
+			body: JSON.stringify({
+				"username": username,
+				"password": password
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			}
+		});
 
-  return (
-    <main>
-      <Head>
-        <title>WhatsFlat Home</title>
-        <meta name="description" content="WhatsFlat Webapp" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+		if (!response.ok) {
+			alert("Login failed due to unknown reasons");
+		} else {
+			const result = Boolean.parse(response.body.toString());
+			alert("Login successful" ? result : "Login failed");
+		}
+	}
 
-      <div className="centered-box">
-        <h1>Welcome to WhatsFlat 📫</h1>
-        <form>
-          <div className="input-with-label">
-            <label>Username:</label>
-            <input
-              type="text"
-              onChange={(element) => setUsername(element.value)}
-            />
-          </div>
+	return (
+		<main>
+			<Head>
+				<title>WhatsFlat Home</title>
+				<meta name="description" content="WhatsFlat Webapp" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-          <div className="input-with-label">
-            <label>Password:</label>
-            <input
-              type="text"
-              onChange={(element) => setPassword(element.value)}
-            />
-          </div>
+			<div className="centered-box">
+				<h1>Welcome to WhatsFlat 📫</h1>
+				<form>
+					<div className="input-with-label">
+						<label>Username:</label>
+						<input
+							type="text"
+							onChange={(element) => setUsername(element.value)}
+						/>
+					</div>
 
-          <button className="login-button" onClick={(event) => login(event)}>
-            Login
-          </button>
-        </form>
-      </div>
-    </main>
-  );
+					<div className="input-with-label">
+						<label>Password:</label>
+						<input
+							type="text"
+							onChange={(element) => setPassword(element.value)}
+						/>
+					</div>
+
+					<button className="login-button" onClick={(event) => login(event)}>
+						Login
+					</button>
+				</form>
+			</div>
+		</main>
+	);
 }
